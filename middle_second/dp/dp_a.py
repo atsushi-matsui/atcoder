ecd310784ec95533fc45c976c320916617c25ef7
atsushi-matsui <kiburedo0702@gmail.com>
2022-02-06 00:02:55 +0900

    commit

---
+++ middle_second/dp/dp_a.py
@@ -0,0 +1,12 @@
+# https://atcoder.jp/contests/dp/tasks/dp_a
+N = int(input())
+h = list(map(int, input().split()))
+
+cost = [0]*N 
+for i in range(1, N):        
+    if i==1:
+        cost[i] = abs(h[i]-h[i-1])+cost[i-1]
+    else:
+        cost[i] = min(abs(h[i]-h[i-1])+cost[i-1], abs(h[i]-h[i-2])+cost[i-2])
+
+print(cost[N-1])
---
+++ middle_second/dp/dp_b.py
@@ -0,0 +1,16 @@
+# https://atcoder.jp/contests/dp/tasks/dp_b
+# 実行環境はpypy3
+N, K = map(int, input().split())
+h = list(map(int, input().split()))
+
+cost = [10**9]*(10**5+1)
+
+# 初期値
+cost[0] = 0
+
+for i in range(1, N):
+    for j in range(1, K+1):
+        if i-j >= 0: 
+            cost[i] = min(cost[i], cost[i-j] + abs(h[i]-h[i-j]))
+
+print(cost[N-1])
