912e7a74a12c41baa7892ca0ba01f5c4137cd480
atsushi-matsui <kiburedo0702@gmail.com>
2022-02-12 02:32:08 +0900

    commit

---
+++ middle_second/dp/tdpc_contest.py
@@ -0,0 +1,31 @@
+# https://atcoder.jp/contests/tdpc/tasks/tdpc_contest
+from unittest import result
+
+
+N = int(input())
+ps = [0]+list(map(int, input().split()))
+
+p = sum(ps)+1
+
+conbine = []
+for i in range(N+1):
+    conbine.append([False]*p)
+
+#初期値
+conbine[0][0] = True
+
+for i in range(1, N+1):
+    for j in range(p):
+        # 解かなかった場合
+        if conbine[i-1][j]:
+           conbine[i][j] = True
+        # 解いた場合
+        if j >=ps[i] and conbine[i-1][j-ps[i]]:
+            conbine[i][j] = True
+
+result = 0
+for con in conbine[N]:
+    if con:
+        result +=1
+
+print(result)
