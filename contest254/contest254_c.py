496017167de75a5035c15e7b02a0dccf1ab7d8f9
atsushi-matsui <kiburedo0702@gmail.com>
2022-06-05 17:16:25 +0900

    fix

--- contest254/contest254_c.py
+++ contest254/contest254_c.py
@@ -3,24 +3,21 @@ A = list(map(int, input().split()))
 
 if K == 1:
     print("Yes")
+    exit()
+
+B = []
+for _ in range(K):
+    B.append(list())
+for i, v in enumerate(A):
+    B[i % K].append(v)
+for b in B:
+    b.sort()
+res = [0] * N
+for i in range(N):
+    res[i] = B[i % K][i // K]
+
+A.sort()
+if A == res:
+    print("Yes")
 else:
-    B = []
-    for _ in range(K):
-        B.append(list())
-
-    for i, v in enumerate(A):
-        B[i % K].append(v)
-
-    for b in B:
-        b.sort()
-
-    res = [0] * N
-    for i in range(N):
-        res[i] = B[i % K][i // K]
-
-    A.sort()
-
-    if A == res:
-        print("Yes")
-    else:
-        print("No")
+    print("No")
