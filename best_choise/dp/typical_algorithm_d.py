348eff7f759fcde7dadb94953798e89bde950243
atsushi-matsui <kiburedo0702@gmail.com>
2022-07-07 08:53:29 +0900

    resolve

--- best_choise/dp/typical_algorithm_d.py
+++ best_choise/dp/typical_algorithm_d.py
@@ -1,47 +1,33 @@
 # https://atcoder.jp/contests/typical-algorithm/tasks/typical_algorithm_d
-"""
-ダイクトラム法
-ヒープキューを利用して解く。
-キューを取得する：O(logN)
-キューを追加する：O(logN)
-"""
-
 import heapq
 
 N, M = map(int, input().split())
-G = []
-for _ in range(M):
-    G.append([])
-
+D = []
+for _ in range(N):
+    D.append([])
 for _ in range(M):
     u, v, c = map(int, input().split())
-    G[u].append((v, c))
+    D[u].append((c, v))
+# iまで到達するための最短コスト
+C = [10**18] * N
+# 訪問済みか
+visited = [False] * N
 
-# 距離
-dist = [-1] * N
-# 訪問済みであるのか
-is_visited = [False] * N
-# (距離、頂点)のキュー
+# キュー
 Q = []
-# 初期化
 heapq.heappush(Q, (0, 0))
-dist[0] = 0
 
 while len(Q) > 0:
-    d, s = heapq.heappop(Q)
+    c, s = heapq.heappop(Q)
 
-    # ヒープキューでは始点からの総距離で昇順にソート済みで、訪問済みの頂点はすでに最短経路を算出済みのため、再計算の必要なし
-    if is_visited[s]:
+    # コストのソート順に取得しているので、一度取り出したことのあるノードで再計算は不要
+    if visited[s]:
         continue
-    is_visited[s] = True
+    visited[s] = True
 
-    for g, c in G[s]:
-        # 到達不可
-        if c == 0:
-            continue
-        # 最小コストに更新
-        if dist[g] == -1 or dist[g] > d + c:
-            dist[g] = d + c
-            heapq.heappush(Q, (dist[g], g))
+    for d, g in D[s]:
+        if C[g] > c + d:
+            C[g] = c + d
+            heapq.heappush(Q, (C[g], g))
 
-print(dist[-1])
+print(C[N - 1])
